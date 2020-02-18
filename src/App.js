81c21f66b23174fe28f714e657818167e8f1344e
João Justo <jpjustonunes@gmail.com>
2020-02-18 22:02:29 +0100

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Phaser from "phaser";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+
+function preload() {
+  this.load.setBaseURL("http://labs.phaser.io");
+
+  this.load.image("sky", "assets/skies/space3.png");
+  this.load.image("logo", "assets/sprites/phaser3-logo.png");
+  this.load.image("red", "assets/particles/red.png");
+}
+
+function create() {
+  this.add.image(400, 300, "sky");
+
+  var particles = this.add.particles("red");
+
+  var emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: "ADD"
+  });
+
+  var logo = this.physics.add.image(400, 100, "logo");
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
+}
+
+const game = new Phaser.Game(config);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
