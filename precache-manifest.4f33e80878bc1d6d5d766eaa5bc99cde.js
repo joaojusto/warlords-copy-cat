d7770455f785ae29eddb3b17285ab8b6ac5baf4a
João Justo <jpjustonunes@gmail.com>
2020-02-18 22:07:03 +0100

    Updates

---
+++ precache-manifest.4f33e80878bc1d6d5d766eaa5bc99cde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f87abd9360257c521c7ac088c1ae6f9",
+    "url": "/warlords-copy-cat/index.html"
+  },
+  {
+    "revision": "abd27783e0a002656d39",
+    "url": "/warlords-copy-cat/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a91e8f4357a004bc57a6",
+    "url": "/warlords-copy-cat/static/js/2.ca21f9a4.chunk.js"
+  },
+  {
+    "revision": "a7fde2a9b97aa77183f520e44a7409c1",
+    "url": "/warlords-copy-cat/static/js/2.ca21f9a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd27783e0a002656d39",
+    "url": "/warlords-copy-cat/static/js/main.8252a7cd.chunk.js"
+  },
+  {
+    "revision": "a40dcda68720a88c6811",
+    "url": "/warlords-copy-cat/static/js/runtime-main.0150e67d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/warlords-copy-cat/static/media/logo.5d5d9eef.svg"
+  }
+]);
